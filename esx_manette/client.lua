053e7c4165ceef0f8a1cc9c9932895160ae9509b
XenoXzTV <90718666+XenoXzTV@users.noreply.github.com>
2021-11-22 20:32:05 +0100

    Update client.lua

--- esx_manette/client.lua
+++ esx_manette/client.lua
@@ -31,19 +31,3 @@ AddEventHandler('esx_manette:ammanetta', function()
 	end
 
 end)
-
-ESX.RegisterUsableItem('manette', function()
-    local target, , closestPlayer, distance = ESX.Game.GetClosestPlayer()
-    playerheading = GetEntityHeading(PlayerPedId())
-    playerlocation = GetEntityForwardVector(PlayerPedId())
-    playerCoords = GetEntityCoords(PlayerPedId())
-    Citizen.Wait(250)
-    loadanimdict('mp_arresting')
-    TaskPlayAnim(PlayerPedId(), 'mp_arresting', 'a_uncuff', 15.0, -8,-1, 2, 0, 0, 0, 0)
-    Citizen.Wait(2000)
-    ClearPedTasks(PlayerPedId())
-    local target_id = GetPlayerServerId(target)
-    if distance <= 2.0 then
-        TriggerServerEvent('esx_manette:ammanetta', GetPlayerServerId(closestPlayer))
-    end
-end)
